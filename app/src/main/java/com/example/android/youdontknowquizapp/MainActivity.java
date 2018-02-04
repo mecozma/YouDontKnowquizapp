d738074e811a764900ccbcf5a51f9c83eab75d4f
mecozma <mecozma@gmail.com>
2018-02-04 05:29:01 +0000

    Initial commit

---
+++ app/src/main/java/com/example/android/youdontknowquizapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.android.youdontknowquizapp;
+
+
+
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.security.DigestOutputStream;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
